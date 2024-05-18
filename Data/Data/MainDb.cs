e668042a684d2595550d6c1827236eec398b5417
Artuross22 <bonjjjuaro@gmail.com>
2024-05-18 23:49:14 +0300

    Add project files.

---
+++ Data/Data/MainDb.cs
@@ -0,0 +1,14 @@
+﻿using Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Db;
+
+public class MainDb : DbContext
+{
+    public MainDb(DbContextOptions<MainDb> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Asset> Assets { get; set; }
+}
---
+++ Data/Models/Asset.cs
@@ -0,0 +1,22 @@
+﻿namespace Data.Models;
+
+public class Asset
+{
+    public int Id { get; set; }
+
+    public Guid PublicId { get; set; }
+
+    public required string Name { get; set; }
+
+    public decimal CurrentPrice { get; set; }
+
+    public decimal BoughtFor { get; set; }
+
+    public DateTime AddedAt { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public DateTime? ClosedAt { get; set; }
+
+    public decimal? SoldFor { get; set; }
+}
