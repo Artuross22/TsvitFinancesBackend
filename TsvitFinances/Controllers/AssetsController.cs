9408b53113efe374bd386d94aeed77c3fe2a9a10
Artuross22 <bonjjjuaro@gmail.com>
2024-05-19 17:32:57 +0300

    Added CRUD for asset

---
+++ TsvitFinances/Controllers/AssetsController.cs
@@ -0,0 +1,91 @@
+﻿using Data.Db;
+using Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace TsvitFinances.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AssetsController : Controller
+    {
+        readonly protected MainDb _mainDb;
+
+        public AssetsController(MainDb mainDb)
+        {
+            _mainDb = mainDb;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Asset>>> GetAssets()
+        {
+            return await _mainDb.Assets.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Asset>> GetAsset(int id)
+        {
+            var product = await _mainDb.Assets.FirstOrDefaultAsync(a => a.Id == id);
+
+            if (product is null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Asset>> AddAsset([FromBody] Asset asset)
+        {
+            _mainDb.Assets.Add(asset);
+            await _mainDb.SaveChangesAsync();
+
+            return Ok(await GetAssets());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Asset>> UpdateAsset([FromBody] Asset asset)
+        {
+            var dbAsset = await _mainDb.Assets
+                 .Where(a => a.Id == asset.Id)
+                 .SingleOrDefaultAsync();
+
+            if (dbAsset is null)
+            {
+                return NotFound();
+            }
+
+            dbAsset.Name = asset.Name;
+            dbAsset.CurrentPrice = asset.CurrentPrice;
+            dbAsset.AddedAt = asset.AddedAt;
+            dbAsset.ClosedAt = asset.ClosedAt;
+            dbAsset.IsActive = asset.IsActive;
+            dbAsset.BoughtFor = asset.BoughtFor;
+            dbAsset.SoldFor = asset.SoldFor;
+
+            await _mainDb.SaveChangesAsync();
+
+            return Ok(await GetAssets());
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<Asset>> DeleteAsset(int id)
+        {
+            var dbAsset = await _mainDb.Assets
+                 .Where(a => a.Id == id)
+                 .SingleOrDefaultAsync();
+
+            if (dbAsset is null)
+            {
+                return NotFound();
+            }
+
+            _mainDb.Remove(dbAsset);
+            await _mainDb.SaveChangesAsync();
+
+            return Ok(await GetAssets());
+        }
+    }
+}
---
+++ TsvitFinances/Dto/AssetDto.cs
@@ -0,0 +1,21 @@
+﻿namespace TsvitFinances.Dto
+{
+    public class AssetDto
+    {
+        public Guid PublicId { get; set; }
+
+        public required string Name { get; set; }
+
+        public decimal CurrentPrice { get; set; }
+
+        public decimal BoughtFor { get; set; }
+
+        public DateTime AddedAt { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public DateTime? ClosedAt { get; set; }
+
+        public decimal? SoldFor { get; set; }
+    }
+}
