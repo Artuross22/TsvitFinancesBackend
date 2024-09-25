cf9c17a127e05bdc904db3ce7e537d587055919b
Artuross22 <bonjjjuaro@gmail.com>
2024-09-25 21:38:42 +0300

    Added DbContextFactory

---
+++ Data/Internal/DbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Data.Db;
+
+namespace Data.Internal;
+
+public class DbContextFactory : IDesignTimeDbContextFactory<MainDb>
+{
+    public MainDb CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+         .SetBasePath(Directory.GetCurrentDirectory())
+         .AddJsonFile("appsettings.json")
+         .Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<MainDb>();
+        return(new MainDb(optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection")).Options));
+    }
+}
